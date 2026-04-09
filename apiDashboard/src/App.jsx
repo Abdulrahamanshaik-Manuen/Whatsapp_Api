cabb8396a4e1d410da14e3b18ae1aae692c07838
Abdulrahamanshaik-Manuen <Abdulrahamanshaik@manuen.com>
2026-04-09 16:38:48 +0530

    Initial commit

---
+++ apiDashboard/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white p-10">
+      <h1 className="text-4xl font-bold text-green-600">Tailwind is working</h1>
+    </div>
+  )
+}
